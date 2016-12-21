9fe0f176edde59b26c3778f8d674409ceca3cf67
Kathleen Catlett <kathleencatlett@comcast.net>
2016-12-21 13:06:56 -0500

    that is all I can do until I see the specs

---
+++ assets/javascript/trainSchedule.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBB7-HCnEi21ox3JuxWgjIHCmfFnUgPGBI",
+    authDomain: "train-schedule-66db2.firebaseapp.com",
+    databaseURL: "https://train-schedule-66db2.firebaseio.com",
+    storageBucket: "train-schedule-66db2.appspot.com",
+    messagingSenderId: "743138719065"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
+
+
+
